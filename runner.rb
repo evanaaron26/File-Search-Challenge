4c9c10d60ed8d840417bfb7acf2b02e439dc39b2
evanaaron26 <evanaaron26@yahoo.com>
2017-04-02 13:06:35 -0500

    second commit

---
+++ runner.rb
@@ -0,0 +1,19 @@
+puts "Welcome to File Search Challenge. 
+Type a number to make a choice from the list."
+
+puts ""
+
+puts "Target Tables"
+
+puts "-------------"
+
+puts "0 - Exit"
+
+puts "1 - investors"
+
+puts "2 - accounts"
+
+puts "3 - documents"
+
+puts "Please select a table"
+
